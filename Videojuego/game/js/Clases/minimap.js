12fdb1ed0e60c85865db9294389b02c7d3c885d9
diegocordova <A01781166@tec.mx>
2025-03-28 12:35:54 -0600

    Comentarios de minimapa arreglados

--- Videojuego/game/js/Clases/minimap.js
+++ Videojuego/game/js/Clases/minimap.js
@@ -1,55 +1,65 @@
 class MiniMap extends GameObject {
     constructor(_color, width, height, x, y, _type) {
         super(_color, width, height, x, y, "minimap");
-
-        this.isDrawn = false;
     }
 
     draw(ctx, rooms, currentRoomId) {
-        // Dibuja el fondo del minimapa
-        ctx.fillStyle = "rgba(0, 0, 0, 0.5)"; // Fondo semitransparente
+        // Draw the background of the minimap
+        ctx.fillStyle = "rgba(0, 0, 0, 0.5)"; // Semi-transparent black
         ctx.fillRect(this.position.x, this.position.y, this.size.x, this.size.y);
 
-        // Tamaño de las salas
+        // Define the size of the rooms
         const roomWidth = this.size.x / numRooms;
         const roomHeight = this.size.y / 3;
         const spacing = 0; // Espaciado entre salas
 
-        // Dibuja las salas principales y sus ramas
+        // Draw the rooms
         for (let i = 0; i < numRooms; i++) {
+
+            // Get the current room
             const room = rooms.get(i);
+            // Define the position of the room according to the index
             const roomX = this.position.x + i * roomWidth;
             const roomY = this.position.y + this.size.y / 3;
 
-            // Dibuja la sala principal
+            // Draw the current room
             this.drawRoomRect(ctx, room, roomX, roomY, roomWidth - spacing, roomHeight - spacing);
 
-            // Si es la sala actual, dibuja un rectángulo más pequeño dentro
+            // If the player is in the room, draw the player rectangle
             if (room.id === currentRoomId) {
-                room.isExplored = true; // Marca la sala como explorada
+                // Set the room as explored
+                room.isExplored = true;
                 this.drawPlayerRect(ctx, room, roomX, roomY, roomWidth - spacing, roomHeight - spacing);
             }
 
-            // Dibuja las ramas conectadas
+            // If the room has branches, draw them
             for (const connectionId of room.connections) {
+                // Get the connected room
                 const connectedRoom = rooms.get(connectionId);
-                const branchX = roomX; // Mantiene la misma posición X
+                // Mantain the same position X
+                const branchX = roomX;
 
+                // If the room is above, draw it
                 if (connectedRoom.type === "button"
                     || connectedRoom.type === "branch1") {
-                    // Dibuja la rama arriba
-                    const branchY = roomY - roomHeight - spacing; // Se mueve hacia arriba
+                    // Move the room up
+                    const branchY = roomY - roomHeight - spacing;
+                    // Draw the room
                     this.drawRoomRect(ctx, connectedRoom, branchX, branchY, roomWidth - spacing, roomHeight - spacing);
+                    // Check if the player is in the room
                     if (connectedRoom.id === currentRoomId) {
-                        connectedRoom.isExplored = true; // Marca la sala como explorada
+                        connectedRoom.isExplored = true;
                         this.drawPlayerRect(ctx, connectedRoom, branchX, branchY, roomWidth - spacing, roomHeight - spacing);
                     }
+                // If the room is below, draw it
                 } else if (connectedRoom.type === "branch2") {
-                    // Dibuja la rama abajo
+                    // Move the room down
                     const branchY = roomY + roomHeight + spacing;
+                    // Draw the room
                     this.drawRoomRect(ctx, connectedRoom, branchX, branchY, roomWidth - spacing, roomHeight - spacing);
+                    // Check if the player is in the room
                     if (connectedRoom.id === currentRoomId) {
-                        connectedRoom.isExplored = true; // Marca la sala como explorada
+                        connectedRoom.isExplored = true;
                         this.drawPlayerRect(ctx, connectedRoom, branchX, branchY, roomWidth - spacing, roomHeight - spacing);
                     }
                 }
@@ -57,41 +67,51 @@ class MiniMap extends GameObject {
         }
     }
 
+    // Draw the room rectangle
     drawRoomRect(ctx, room, x, y, width, height, color) {
 
+        // If the color is defined, use it
         if (color !== undefined) {
             ctx.fillStyle = color; // Color de la sala
         }
+        // If the room is explored, use the color set
         else if (!room.isExplored) {
             if (room.type === "boss") {
+                // If the room has a boss, check if its unlocked
                 if (game.isButtonPressed) {
-                    ctx.fillStyle = "lightgreen"; // Color de la sala de jefe
+                    ctx.fillStyle = "lightgreen";
                 } else {
-                    ctx.fillStyle = "darkred"; // Color de la sala de jefe
+                    ctx.fillStyle = "darkred";
                 }
+            // If the room is not explored
             } else {
-                ctx.fillStyle = "gray"; // Color de la sala no explorada
+                ctx.fillStyle = "gray";
             }
         }
+        // If the room is not explored, use default colors
         else {
             if (room.type === "button") {
-                ctx.fillStyle = "lightblue"; // Color de la sala de botón
+                ctx.fillStyle = "lightblue";
             } else {
-                ctx.fillStyle = "white"; // Color de la sala normal
+                ctx.fillStyle = "white";
             }
         }
 
-        ctx.fillRect(x, y, width, height); // Dibuja la sala
-        ctx.strokeStyle = "black"; // Color del borde
-        ctx.lineWidth = 2; // Grosor del borde
-        ctx.strokeRect(x, y, width, height); // Dibuja el borde
+        // Fill the room rectangle
+        ctx.fillRect(x, y, width, height);
+        // Add a border
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 2;
+        ctx.strokeRect(x, y, width, height);
     }
 
+    // Draw the player rectangle
     drawPlayerRect(ctx, room, x, y, width, height) {
-        const innerRectWidth = width * 0.5; // 50% del ancho
-        const innerRectHeight = height * 0.5; // 50% del alto
-        const innerRectX = x + (width - innerRectWidth) / 2; // Centrado
-        const innerRectY = y + (height - innerRectHeight) / 2; // Centrado
+        const innerRectWidth = width * 0.5; // 50% of the width
+        const innerRectHeight = height * 0.5; // 50% of the height
+        const innerRectX = x + (width - innerRectWidth) / 2; // Centered
+        const innerRectY = y + (height - innerRectHeight) / 2; // Centered
+        // Draw a rectangle in the center of the room
         this.drawRoomRect(ctx, room, innerRectX, innerRectY, innerRectWidth, innerRectHeight, "lightgreen");
     }
 }
